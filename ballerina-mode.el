4f07f78a05860cecff96246b972f2e8d6511b181
Heshan Padmasiri <heshanp@wso2.com>
2023-07-22 07:22:12 +0530

    Move the original code

--- ballerina-mode.el
+++ ballerina-mode.el
@@ -1,4 +1,4 @@
-;;; ballerina-mode.el -- A major mode for editing ballerina source code -*- lexical-binding: t; -*-
+;;; ballerina-mode.el --- A major mode for editing ballerina source code -*- lexical-binding: t; -*-
 ;;
 ;; Copyright (C) 2023 Heshan Padmasiri
 ;;
@@ -15,11 +15,97 @@
 ;;
 ;;; Commentary:
 ;;
-;;
+;; This file implements a major mode for editing ballerina code
 ;;
 ;;; Code:
 
 
+(defconst ballerina-mode-keywords
+  '("ascending"
+    "break"
+    "by"
+    "check"
+    "checkpanic"
+    "class"
+    "client"
+    "conflict"
+    "const"
+    "descending"
+    "distinct"
+    "else"
+    "equals"
+    "final"
+    "foreach"
+    "from"
+    "function"
+    "if"
+    "import"
+    "in"
+    "isolated"
+    "join"
+    "let"
+    "limit"
+    "on"
+    "order"
+    "outer"
+    "private"
+    "public"
+    "readonly"
+    "remote"
+    "return"
+    "returns"
+    "select"
+    "service"
+    "type"
+    "var"
+    "where"
+    "while")  "All keywords in the ballerina language.  Used for font locking.")
+
+(defvar ballerina-mode-syntax-table
+  (let ((table (make-syntax-table)))
+    ;; punctuations
+    (modify-syntax-entry ?+ "." table)
+    (modify-syntax-entry ?- "." table)
+    (modify-syntax-entry ?/ "." table)
+    (modify-syntax-entry ?* "." table)
+    (modify-syntax-entry ?% "." table)
+    (modify-syntax-entry ?= "." table)
+    (modify-syntax-entry ?! "." table)
+    (modify-syntax-entry ?< "." table)
+    (modify-syntax-entry ?> "." table)
+    ;; Strings
+    (modify-syntax-entry ?\" "\"" table)
+    (modify-syntax-entry ?\\ "\\" table)
+
+    ;; whitespaces
+
+    ;; Comments
+    (modify-syntax-entry ?/  ". 124b" table)
+    (modify-syntax-entry ?*  ". 23n"  table)
+    (modify-syntax-entry ?\n "> b"    table)
+    (modify-syntax-entry ?\^m "> b"   table)
+    table)
+  "Syntax definitions and helpers.")
+
+(defun ballerina-build-font-lock-keywords ()
+  (append
+   `((, (concat "\\_<" (regexp-opt ballerina-mode-keywords t) "\\_>") . font-lock-keyword-face))))
+
+;;;###autoload
+(define-derived-mode ballerina-mode prog-mode "Ballerina"
+   "Ballerina mode is a major mode for editing ballerina files"
+   ;; Syntax highlighting
+   :syntax-table ballerina-mode-syntax-table
+   ;; keybindings
+   ;; (define-key ballerina-mode-map (kbd "C-c C-f") 'ballerina-mode-command)
+   ;; Fonts
+        (setq font-lock-defaults '(ballerina-build-font-lock-keywords))   ;; Indentation settings
+   (setq-local indent-tabs-mode nil) ; use spaces for indentation
+   (setq-local tab-width 4) ; tabwidth 4 spaces
+   (setq-local electric-indent-inhibit t)) ; Disabel electric indentation
+
+;;;###autoload
+(add-to-list 'auto-mode-alist '("\\.bal\\'" . ballerina-mode))
 
 (provide 'ballerina-mode)
 ;;; ballerina-mode.el ends here
