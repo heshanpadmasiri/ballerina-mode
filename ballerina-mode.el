06311d8e5da71991b1dd71aeed3926b459d962f4
Heshan Padmasiri <heshanp@wso2.com>
2023-07-22 07:19:50 +0530

    Initial commit

---
+++ ballerina-mode.el
@@ -0,0 +1,25 @@
+;;; ballerina-mode.el -- A major mode for editing ballerina source code -*- lexical-binding: t; -*-
+;;
+;; Copyright (C) 2023 Heshan Padmasiri
+;;
+;; Author: Heshan Padmasiri <hpheshan@gmail.com>
+;; Maintainer: Heshan Padmasiri <hpheshan@gmail.com>
+;; Created: July 22, 2023
+;; Modified: July 22, 2023
+;; Version: 0.0.1
+;; Keywords: ballerina languages
+;; Homepage: https://github.com/heshanpadmasiri/ballerina-mode
+;; Package-Requires: ((emacs "24.3"))
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; Commentary:
+;;
+;;
+;;
+;;; Code:
+
+
+
+(provide 'ballerina-mode)
+;;; ballerina-mode.el ends here
