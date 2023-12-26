e48f49ad2e12c909edee64afb3d4240631335b57
Heshan Padmasiri <heshanp@wso2.com>
2023-12-26 12:25:58 +0530

    Always run bal commands from the project root (#6)
    * Factor out compile commands
    
    This allows us to change the exact ballerina compiler we are using
    
    * Add ability to find project root
    
    When executing commands always try to find the parent with
        "Ballerina.toml" file and execute `bal` commands from that directory

--- ballerina-mode.el
+++ ballerina-mode.el
@@ -5,7 +5,7 @@
 ;; Author: Heshan Padmasiri <hpheshan@gmail.com>
 ;; Maintainer: Heshan Padmasiri <hpheshan@gmail.com>
 ;; Created: July 22, 2023
-;; Modified: December 24, 2023
+;; Modified: December 26, 2023
 ;; Version: 0.0.1
 ;; Keywords: ballerina languages
 ;; Homepage: https://github.com/heshanpadmasiri/ballerina-mode
@@ -21,70 +21,70 @@
 (require 'reformatter)
 (defconst ballerina-mode-basic-types
   '("any"
-     "anydata"
-     "boolean"
-     "byte"
-     "decimal"
-     "error"
-     "float"
-     "function"
-     "future"
-     "handle"
-     "int"
-     "json"
-     "map"
-     "never"
-     "nil"
-     "object"
-     "object"
-     "readonly"
-     "record"
-     "stream"
-     "string"
-     "table"
-     "typedesc") "Basic types in ballerina language used for font locking.")
+    "anydata"
+    "boolean"
+    "byte"
+    "decimal"
+    "error"
+    "float"
+    "function"
+    "future"
+    "handle"
+    "int"
+    "json"
+    "map"
+    "never"
+    "nil"
+    "object"
+    "object"
+    "readonly"
+    "record"
+    "stream"
+    "string"
+    "table"
+    "typedesc") "Basic types in ballerina language used for font locking.")
 
 (defconst ballerina-mode-keywords
   '("ascending"
-     "break"
-     "by"
-     "check"
-     "checkpanic"
-     "class"
-     "client"
-     "conflict"
-     "const"
-     "descending"
-     "distinct"
-     "else"
-     "equals"
-     "final"
-     "foreach"
-     "from"
-     "function"
-     "if"
-     "import"
-     "in"
-     "is"
-     "isolated"
-     "join"
-     "let"
-     "limit"
-     "match"
-     "on"
-     "order"
-     "outer"
-     "private"
-     "public"
-     "remote"
-     "return"
-     "returns"
-     "select"
-     "service"
-     "type"
-     "var"
-     "where"
-     "while")  "All keywords in the ballerina language.  Used for font locking.")
+    "break"
+    "by"
+    "check"
+    "checkpanic"
+    "class"
+    "client"
+    "conflict"
+    "const"
+    "descending"
+    "distinct"
+    "else"
+    "equals"
+    "final"
+    "foreach"
+    "from"
+    "function"
+    "if"
+    "import"
+    "in"
+    "is"
+    "isolated"
+    "join"
+    "let"
+    "limit"
+    "match"
+    "on"
+    "order"
+    "outer"
+    "private"
+    "public"
+    "remote"
+    "return"
+    "returns"
+    "select"
+    "service"
+    "type"
+    "var"
+    "where"
+    "while")  "All keywords in the ballerina language.  Used for font locking.")
 
 ;; TODO: currently does nothing (mostly to shutup copilot)
 (defcustom ballerina-indent-offset 4
@@ -117,8 +117,8 @@
 (defconst ballerina-func-regexp (concat "\\_<function\\_>\\s *\\(" ballerina-identifier-regexp "\\)"))
 (defconst ballerina-return-type-regexp (concat "\\_<returns\\_>\\s *\\(" ballerina-type-regexp "\\)"))
 (defconst ballerina-var-type-regexp (concat "\\(" ballerina-type-regexp "\\)"
-                                      "[[:space:]][[:space:]]*"
-                                      "\\(" ballerina-identifier-regexp"\\)"))
+                                            "[[:space:]][[:space:]]*"
+                                            "\\(" ballerina-identifier-regexp"\\)"))
 
 (defvar ballerina-mode-syntax-table
   (let ((table (make-syntax-table)))
@@ -148,12 +148,45 @@
 
 (defun ballerina-build-font-lock-keywords ()
   (append
-    `((, (concat "\\_<" (regexp-opt ballerina-mode-basic-types t) "\\_>") . font-lock-type-face)
-       (, (concat "\\_<" (regexp-opt ballerina-mode-keywords t) "\\_>") . font-lock-keyword-face)
-       (, ballerina-func-regexp 1 font-lock-function-name-face)
-       (, ballerina-return-type-regexp 1 font-lock-type-face)
-       (, ballerina-var-type-regexp 1 font-lock-type-face)
-       (, ballerina-var-type-regexp 2 font-lock-variable-name-face))))
+   `((, (concat "\\_<" (regexp-opt ballerina-mode-basic-types t) "\\_>") . font-lock-type-face)
+     (, (concat "\\_<" (regexp-opt ballerina-mode-keywords t) "\\_>") . font-lock-keyword-face)
+     (, ballerina-func-regexp 1 font-lock-function-name-face)
+     (, ballerina-return-type-regexp 1 font-lock-type-face)
+     (, ballerina-var-type-regexp 1 font-lock-type-face)
+     (, ballerina-var-type-regexp 2 font-lock-variable-name-face))))
+
+(defcustom ballerina-bal-bin "bal"
+  "Path to ballerina executable."
+  :type 'string
+  :group 'ballerina-mode)
+
+(defvar-local ballerina-buffer-project nil)
+
+(defun ballerina-update-buffer-project ()
+  (setq-local ballerina-buffer-project (ballerina-find-project-directory)))
+
+(defun ballerina-find-project-directory ()
+  "Find the nearest directory containing a Ballerina.toml file."
+  (let ((current-dir (file-name-directory buffer-file-name))
+        (continue t))
+    (while (and current-dir continue)
+      (let ((toml-path (expand-file-name "Ballerina.toml" current-dir)))
+        (if (file-exists-p toml-path)
+            (setq continue nil)
+          (let ((parent-dir (file-truename (concat current-dir "../"))))
+            (if (string= current-dir parent-dir)
+                (setq current-dir nil)
+              (setq current-dir parent-dir))))))
+    current-dir))
+
+(defun ballerina--compile (format-string &rest args)
+  (when (null ballerina-buffer-project)
+    (ballerina-update-buffer-project))
+  (let ((default-directory
+         (or (and ballerina-buffer-project
+                  (file-name-directory ballerina-buffer-project))
+             default-directory)))
+    (compile (apply #'format format-string args))))
 
 ;;;###autoload
 (defun ballerina-mode-test ()
@@ -161,21 +194,21 @@
   (interactive)
   (let ((user-input (read-string "Enter test selection (leave empty to run all tests): ")))
     (if (string-empty-p user-input)
-      (compile "bal test")
-      (compile (format "bal test --tests %s" user-input)))))
+        (ballerina--compile "%s test" ballerina-bal-bin)
+      (ballerina--compile "%s test --tests %s" ballerina-bal-bin user-input))))
 
 ;;;###autoload
 (defun ballerina-mode-run ()
   "Run current ballerina file"
   (interactive)
   (let ((file-name (buffer-file-name)))
-    (compile (format "bal run %s" file-name))))
+    (ballerina--compile "%s run %s" ballerina-bal-bin file-name)))
 
 ;;;###autoload
 (defun ballerina-mode-build ()
   "Build current ballerina project"
   (interactive)
-  (compile "bal build"))
+  (ballerina--compile "%s build" ballerina-bal-bin))
 
 (defvar ballerina-mode-map
   (let ((map (make-sparse-keymap)))
@@ -202,9 +235,9 @@
   (setq-local comment-end "")
   ;; indentation
   (setq-local electric-indent-chars
-    (append ballerina-electric-indent-chars
-      (and (boundp 'electric-indent-chars)
-        electric-indent-chars))))
+              (append ballerina-electric-indent-chars
+                      (and (boundp 'electric-indent-chars)
+                           electric-indent-chars))))
 
 ;;;###autoload
 (add-to-list 'auto-mode-alist '("\\.bal\\'" . ballerina-mode))
